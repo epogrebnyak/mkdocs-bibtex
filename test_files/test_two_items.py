b9c6fdc18138527065971820d9ee737cc2783e18
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2022-02-12 14:46:14 +0000

    test_two_items.py

--- test_files/test_two_items.py
+++ test_files/test_two_items.py
@@ -1,16 +1,17 @@
 import os
+from mkdocs_bibtex import BibTexPlugin
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
-test_files_dir = os.path.abspath(os.path.join(module_dir, "..", "test_files"))
 
 def test_on_2_items():
   plugin = BibTexPlugin()
   plugin.load_config(
-        options={"bib_file": os.path.join(test_files_dir, "paper.bib")},
+        options={"bib_file": os.path.join(module_dir, "paper.bib")},
         config_file_path=None,
     )
   plugin.on_config(plugin.config)
   test_markdown = "Book of Why [@PM18] and Time Series Analysis [@Hamilton]\n\n \\bibliography"
+  # ensure there are two items in bibliography
   assert (
         "[^2]:" in plugin.on_page_markdown(test_markdown, None, None, None)
     )
